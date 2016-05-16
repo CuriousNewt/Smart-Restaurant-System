e326be5c61dfeb22729785741b815a7fe81e2990
Adam Minařík <adam.minarik1@gmail.com>
2016-05-16 12:06:45 +0200

    Moved ServerGUI to view

--- sepko2/src/View/ServerGUI.java
+++ sepko2/src/View/ServerGUI.java
@@ -1,6 +1,25 @@
-package sepko2;
+package View;
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
-public class Server extends JFrame {
+public class ServerGUI extends JFrame {
 
+	
+
+public ServerGUI() throws Exception{
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setTitle("SEP Restaurant System");
+    setLayout(new BorderLayout());
+    pack();
+}
+	
+	
+	
+	
+public static void main(String[] args) throws Exception {
+	ServerGUI gui = new ServerGUI();
+    gui.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    gui.setVisible(true);
+}	
 }
