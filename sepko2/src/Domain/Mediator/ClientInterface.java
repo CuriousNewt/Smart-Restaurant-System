75a8f3c4328c90ba3b73903465373a72d88de4f2
st4mpik <st4mpik@gmail.com>
2016-05-24 11:32:22 +0200

    remote excpetion

--- sepko2/src/Domain/Mediator/ClientInterface.java
+++ sepko2/src/Domain/Mediator/ClientInterface.java
@@ -1,13 +1,14 @@
 package Domain.Mediator;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 import Domain.Model.Order;
 
 public interface ClientInterface extends Remote {
 	void setID(int ID);
-	public Order getOrders();
-	public void callStaff();
+	public Order getOrders() throws RemoteException;
+	public void callStaff()  throws RemoteException;
 	
 	
 }
