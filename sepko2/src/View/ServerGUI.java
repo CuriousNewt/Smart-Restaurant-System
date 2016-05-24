8805f5079a891ad05c8f58c480557f5a5b5964b6
Adam Minařík <adam.minarik1@gmail.com>
2016-05-24 12:13:59 +0200

    asd

--- sepko2/src/View/ServerGUI.java
+++ sepko2/src/View/ServerGUI.java
@@ -165,7 +165,7 @@ public class ServerGUI extends JFrame {
 
 	public void updateListofOrders() {
 		for(int i=0;i<controller.getOrders().show().size();i++){
-			ordersModel.add(i, controller.getOrders().show().get(i));
+			ordersModel.add(i, controller.getOrders().show().get(i).toString());
 		}
 		
 	}
