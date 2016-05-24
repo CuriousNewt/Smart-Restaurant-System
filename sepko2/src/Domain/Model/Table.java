70be9b72dcfa16ad66eb85f38edb9af8249f2a33
asus <asus@DESKTOP-2GH04H6>
2016-05-24 14:47:38 +0200

    table orders

--- sepko2/src/Domain/Model/Table.java
+++ sepko2/src/Domain/Model/Table.java
@@ -2,31 +2,32 @@ package Domain.Model;
 
 import java.io.Serializable;
 
-
-public class Table implements Serializable{
+public class Table implements Serializable {
 	private Order order;
 	private int tableNumber;
 
-	
-	
-	public Table(int tableNumber){
+	public Table(int tableNumber) {
 		this.tableNumber = tableNumber;
 		this.order = new Order();
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return "Table number " + tableNumber;
 	}
-	
-	public void addItemToOrder(Item item){
+
+	public void addItemToOrder(Item item) {
 		this.order.addItem(item);
 	}
-	
-	public void removeItem(Item item){
+
+	public void removeItem(Item item) {
 		this.order.removeItem(item);
 	}
-	
-	public Order getOrder(){
+
+	public Order getOrder() {
 		return order;
+	}
+
+	public int getTableNumber() {
+		return tableNumber;
 	}
 }
--- sepko2/src/View/ServerGUI.java
+++ sepko2/src/View/ServerGUI.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
@@ -17,6 +19,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import Controller.Controller;
+import Domain.Model.Table;
 
 public class ServerGUI extends JFrame {
 
@@ -25,7 +28,7 @@ public class ServerGUI extends JFrame {
 	private JMenu topMenu;
 	private JMenuItem menuItemEditMenu;
 	private Controller controller;
-	
+
 	// JPANELS
 	// *********************************************************************
 	private JPanel mainPanel;
@@ -35,17 +38,17 @@ public class ServerGUI extends JFrame {
 	private JPanel eastListPanel;
 	private JPanel westButtonPanel;
 	private JPanel westListPanel;
-	
+
 	// JLABELS
 	// *********************************************************************
-	
+
 	// JBUTTONS
 	// *********************************************************************
 	private JButton ordersEditButton;
 	private JButton payedButton;
 	private JButton setAsBringed;
 	private JButton selectButton;
-	
+
 	// JLISTS & DEFAULT LIST MODELS
 	// *********************************************************************
 	private JList<String> listOfTables;
@@ -56,7 +59,7 @@ public class ServerGUI extends JFrame {
 	public ServerGUI(Controller controller) throws Exception {
 		this.controller = controller;
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	    setJMenuBar(topMenuBar);
+		setJMenuBar(topMenuBar);
 		setTitle("SEP Restaurant System");
 		setLayout(new BorderLayout());
 		setComponents();
@@ -64,18 +67,18 @@ public class ServerGUI extends JFrame {
 		addBorders();
 		addActionListeners();
 		pack();
-		
+
 	}
 
-	private void setComponents() throws Exception{
-		
+	private void setComponents() throws Exception {
+
 		// TOP MENU BAR
 		// **********************************************************************
 		topMenuBar = new JMenuBar();
 		topMenu = new JMenu("Edit");
 		menuItemEditMenu = new JMenuItem("Edit menu..");
 		editMenuGui = new EditMenuGUI();
-		
+
 		// JPANELS
 		// **********************************************************************
 		westPanel = new JPanel();
@@ -104,13 +107,13 @@ public class ServerGUI extends JFrame {
 		// **********************************************************************
 	}
 
-	private void addBorders(){
+	private void addBorders() {
 		mainPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
 		eastPanel.setBorder(BorderFactory.createTitledBorder("ORDERS"));
 		westPanel.setBorder(BorderFactory.createTitledBorder("TABLES"));
-		
+
 	}
-	
+
 	private void addPanelsAndLayouts() {
 
 		// SETTING OF LAYOUTS
@@ -128,7 +131,7 @@ public class ServerGUI extends JFrame {
 		// *********************************************************************
 		mainPanel.add(westPanel);
 		mainPanel.add(eastPanel);
-		
+
 		eastListPanel.add(listOfOrders);
 
 		eastButtonPanel.add(ordersEditButton);
@@ -149,9 +152,10 @@ public class ServerGUI extends JFrame {
 		topMenuBar.add(topMenu);
 		topMenu.add(menuItemEditMenu);
 	}
+
 	// ADDING FUNCTIONS
-			// *********************************************************************
-	class OpenEditMenu implements ActionListener{
+	// *********************************************************************
+	class OpenEditMenu implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			editMenuGui.setVisible(true);
@@ -159,33 +163,72 @@ public class ServerGUI extends JFrame {
 			editMenuGui.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		}
 	}
-	
-	
-	public void addActionListeners(){
+
+	public void addActionListeners() {
 		menuItemEditMenu.addActionListener(new OpenEditMenu());
+		listOfTables.addMouseListener(new ViewTableOrder());
 	}
 
-	public void updateListofOrders(int tableNumber) {
+	public void updateListOfOrders(int tableNumber) {
 		ordersModel.clear();
-		for(int i=0;i<controller.showOrders(tableNumber).size();i++){
+		for (int i = 0; i < controller.showOrders(tableNumber).size(); i++) {
 			ordersModel.add(i, controller.showOrders(tableNumber).get(i));
-			//TODO displaying items and tables
-		}	
-		
+			// TODO displaying items and tables
+		}
+
+	}
+
+	public class ViewTableOrder implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getClickCount() == 2) {
+				JList temp = (JList) e.getSource();
+				Table table = (Table) temp.getSelectedValue();
+				System.out.println(table.getOrder());
+				updateListOfOrders(table.getTableNumber());
+			}
+
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
 	}
 
 	public void callStaff(int ID) {
-		JOptionPane.showMessageDialog(ServerGUI.this,
-				"Table number " + ID + " requests your assistance.");
-		
+		JOptionPane.showMessageDialog(ServerGUI.this, "Table number " + ID
+				+ " requests your assistance.");
+
 	}
 
-	/*public static void main(String[] args) throws Exception {
-		ServerGUI gui = new ServerGUI();
-		gui.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		gui.setVisible(true);
-	}*/
-	public void addTableToList(Object table){
+	/*
+	 * public static void main(String[] args) throws Exception { ServerGUI gui =
+	 * new ServerGUI(); gui.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	 * gui.setVisible(true); }
+	 */
+	public void addTableToList(Object table) {
 		tablesModel.addElement(table);
 	}
 }
